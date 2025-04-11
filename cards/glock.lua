b78bdfde621656f305b165cd60070748bfed2f15
eclipsedsunrise <eclipsedsunrise@gmx.com>
2025-04-10 17:57:03 -0700

    fixed multiple jokey boys glock now resets with the correct message. stork now checks if a played card is debuffed. the old master now checks for all SMODS enhancements, not just vanilla enhancments.

--- cards/glock.lua
+++ cards/glock.lua
@@ -1,12 +1,12 @@
 SMODS.Joker {
     key = "glock",
-    config = { extra = {max_mult = 17, mult = 17, shoot = 1}},
+    config = { extra = {max_mult = 17, mult = 17, shoot = 0, shot = 1}},
     loc_vars = function(self, info_queue, card)
         return {
             vars = {
                 card.ability.extra.max_mult, 
                 card.ability.extra.mult,
-                card.ability.extra.shoot,
+                card.ability.extra.shot,  
             }
         }
     end,
@@ -21,25 +21,37 @@ SMODS.Joker {
     cost = 6,
 
     calculate = function(self, card, context)
+        --decremints the card if it's been used.
+        if context.post_joker
+        or context.before 
+        and not context.blueprint
+        and not self.debuff 
+        and card.ability.extra.shoot == 1 then
+            card.ability.extra.mult = card.ability.extra.mult - card.ability.extra.shoot
+            card.ability.extra.shoot = 0
+        end
+
         if context.joker_main and not self.debuff then
             if card.ability.extra.mult > 0 then
+                card.ability.extra.shoot = 1
                 return {
                     mult = card.ability.extra.mult,
                     card = card,
                     message = localize("k_bang"),
                 }
-                end
-        elseif context.post_joker 
-        and not self.debuff 
-        and not context.blueprint then
-            card.ability.extra.mult = card.ability.extra.mult - card.ability.extra.shoot
-            if card.ability.extra.mult == 0 then
-                card.ability.extra.mult = card.ability.extra.max_mult
-                return {
-                    card = card,
-                    message = localize("k_reloaded"),
-                }
             end
+        end
+
+        -- resets the card if mult is at 0 (mag is empty)
+        if context.post_joker 
+        and not self.debuff 
+        and not context.blueprint 
+        and card.ability.extra.mult <= 0 then
+            card.ability.extra.mult = card.ability.extra.max_mult
+            return {
+                card = card,
+                message = localize("k_reloaded"),
+            }
         end
 
     end
