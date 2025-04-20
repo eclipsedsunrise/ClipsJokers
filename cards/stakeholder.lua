33dc9cdce1a5c697a6bbb031120c2d6cbbe39a02
eclipsedsunrise <eclipsedsunrise@gmx.com>
2025-04-19 20:26:28 -0700

    further tweaks to stakeholder further tweaks

--- cards/stakeholder.lua
+++ cards/stakeholder.lua
@@ -16,25 +16,44 @@ SMODS.Joker {
 
     calculate = function(self, card, context)
         -- begin checking during individual card scoring.
-        if context.before and not context.blueprint then
+        if context.before 
+        and not context.blueprint
+        and G.GAME.dollar_buffer ~= nil then
             card.ability.extra.earn_flag = G.GAME.dollar_buffer
         end
         -- when cards are scored, gain mult
         if context.cardarea == G.play 
         and not self.debuff 
-        and not context.blueprint then
-                if G.GAME.dollar_buffer > card.ability.extra.earn_flag then
-                    card.ability.extra.earn_flag = G.GAME.dollar_buffer
-                    card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.a_mult
-                    return {
-                        message = localize('k_upgrade_ex'),
-                        card = card,
-                        colour = G.C.RED
-                    }
-                end
+        and not context.blueprint
+        and G.GAME.dollar_buffer ~= nil then
+            if G.GAME.dollar_buffer > card.ability.extra.earn_flag then
+                card.ability.extra.earn_flag = G.GAME.dollar_buffer
+                card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.a_mult
+                return {
+                    message = localize('k_upgrade_ex'),
+                    card = card,
+                    colour = G.C.RED
+                }
             end
+        end
+
+        if context.pre_joker
+        and not self.debuff
+        and not context.blueprint
+        and G.GAME.dollar_buffer ~= nil then
+            if G.GAME.dollar_buffer > card.ability.extra.earn_flag then
+                card.ability.extra.earn_flag = G.GAME.dollar_buffer
+                card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.a_mult
+                return {
+                    message = localize('k_upgrade_ex'),
+                    card = card,
+                    colour = G.C.RED
+                }
+            end
+        end
+
         -- apply the scored mult during main joker scoring loop
-        if context.joker_main and card.ability.extra.mult >0 and not self.debuff then
+        if context.joker_main and card.ability.extra.mult > 0 and not self.debuff then
             return {
                 mult = card.ability.extra.mult,
             }
