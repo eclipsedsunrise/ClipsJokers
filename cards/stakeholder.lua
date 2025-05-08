42f038b0ca03eb695f790945fedc9db377ba160d
eclipsedsunrise <eclipsedsunrise@gmx.com>
2025-05-07 21:53:42 -0700

    finally actually fixed stakeholder thank GOD

--- cards/stakeholder.lua
+++ cards/stakeholder.lua
@@ -1,6 +1,6 @@
 SMODS.Joker {
     key = 'stakeholder',
-    config = { extra = { mult = 0, a_mult = 1, earn_flag = 0}},
+    config = { extra = { mult = 0, a_mult = 1, earn_flag = 0, last_card = false}},
     loc_vars = function(self, info_queue, card)
         return { vars = {card.ability.extra.mult, card.ability.extra.a_mult}}
     end,
@@ -20,14 +20,33 @@ SMODS.Joker {
         and not context.blueprint
         and G.GAME.dollar_buffer ~= nil then
             card.ability.extra.earn_flag = G.GAME.dollar_buffer
+            card.ability.extra.last_card = false
         end
         -- when cards are scored, gain mult
-        sendDebugMessage("beginning earning evaluation code, earn_flag="..tostring(card.ability.extra.earn_flag)..", dollar buffer="..tostring(G.GAME.dollar_buffer),"MySummerDebugger")
+        -- sendDebugMessage("beginning earning evaluation code, earn_flag="..tostring(card.ability.extra.earn_flag)..", dollar buffer="..tostring(G.GAME.dollar_buffer),"MySummerDebugger")
         if (context.main_scoring or context.individual or context.post_trigger)
         and context.cardarea == G.play
         and not self.debuff
         and not context.blueprint
         and G.GAME.dollar_buffer ~= nil then
+            sendDebugMessage("checking earn flag vs dollar buffer","MySummerDebugger")
+            if G.GAME.dollar_buffer > card.ability.extra.earn_flag then
+                sendDebugMessage("hit!", "MySummerDebugger")
+                card.ability.extra.earn_flag = G.GAME.dollar_buffer
+                card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.a_mult
+                return {
+                    message = localize('k_upgrade_ex'),
+                    card = card,
+                    colour = G.C.RED
+                }
+            end
+        end
+
+        if context.cardarea == G.hand 
+        and context.individual 
+        and not context.end_of_round 
+        and card.ability.extra.last_card == false then
+            sendDebugMessage("last_card corner case scoring context","MySummerDebugger")
             if G.GAME.dollar_buffer > card.ability.extra.earn_flag then
                 sendDebugMessage("hit!", "MySummerDebugger")
                 card.ability.extra.earn_flag = G.GAME.dollar_buffer
